29a8af6551f7617e0670f9d1eeffd5b478f10d41
gautegam <gaute@gamnes.no>
2016-08-15 18:43:23 +0200

    Working mic input and noise/eq level fix. Downsized to arduino scales

--- main.c
+++ main.c
@@ -51,6 +51,23 @@ static uint32_t                m_adc_evt_counter;
 
 static float testInput_f32_9khz[SAMPLES_IN_BUFFER * 2];
 static float testOutput[SAMPLES_IN_BUFFER];
+static uint32_t testOutputArduinoAttmpt[SAMPLES_IN_BUFFER/2];  // Only the first half of bins are usefull
+
+// This is low-level noise that's subtracted from each FFT output column:
+static const uint8_t noise[64] = {
+    8,6,6,5,3,4,4,4,3,4,4,3,2,3,3,4,
+    2,1,2,1,3,2,3,2,1,2,3,1,2,3,4,4,
+    3,2,2,2,2,2,2,1,3,2,2,2,2,2,2,2,
+    2,2,2,2,2,2,2,2,2,2,2,2,2,3,3,4
+};
+
+  // These are scaling quotients for each FFT output column, sort of a
+  // graphic EQ in reverse.  Most music is pretty heavy at the bass end.
+static const uint8_t eq[64]={
+    255, 175,218,225,220,198,147, 99, 68, 47, 33, 22, 14,  8,  4,  2,
+      0,   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+      0,   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+      0,   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 };
 
 //extern float testInput_f32_10khz[SAMPLES_IN_BUFFER * 2];  // For testing
 
@@ -105,7 +122,7 @@ void saadc_sampling_event_init(void)
 
     /* setup m_timer for compare event every 1ms */
     //uint32_t ticks = nrf_drv_timer_ms_to_ticks(&m_timer, 400);
-    //uint32_t ticks = nrf_drv_timer_ms_to_ticks(&m_timer, 2);
+    //uint32_t ticks = nrf_drv_timer_ms_to_ticks(&m_timer, 10);
     uint32_t ticks = nrf_drv_timer_us_to_ticks(&m_timer, 104);  // Every 104 uS should give roughly 9615 Hz
     nrf_drv_timer_extended_compare(&m_timer, NRF_TIMER_CC_CHANNEL0, ticks, NRF_TIMER_SHORT_COMPARE0_CLEAR_MASK, false);
     nrf_drv_timer_enable(&m_timer);
@@ -137,12 +154,12 @@ void saadc_callback(nrf_drv_saadc_evt_t const * p_event)
         err_code = nrf_drv_saadc_buffer_convert(p_event->data.done.p_buffer, SAMPLES_IN_BUFFER);
         APP_ERROR_CHECK(err_code);
 
-        printf("ADC event number: %d\r\n",(int)m_adc_evt_counter);
+        //printf("ADC event number: %d\r\n",(int)m_adc_evt_counter);
         samples_ready = true;
-        //for (int i = 30; i < SAMPLES_IN_BUFFER; i++)
-        //{
-        //    printf("%d:%d\r\n", i, p_event->data.done.p_buffer[i]);
-        //}
+//        for (int i = 0; i < SAMPLES_IN_BUFFER; i++)
+//        {
+//            printf("%d:%d\r\n", i, p_event->data.done.p_buffer[i]);
+//        }
         m_adc_evt_counter++;
     }
 }
@@ -151,7 +168,17 @@ void saadc_init(void)
 {
     ret_code_t err_code;
     nrf_saadc_channel_config_t channel_config =
-            NRF_DRV_SAADC_DEFAULT_CHANNEL_CONFIG_SE(NRF_SAADC_INPUT_AIN0);
+    //        NRF_DRV_SAADC_DEFAULT_CHANNEL_CONFIG_SE(NRF_SAADC_INPUT_AIN0);
+    {                                                      
+    .resistor_p = NRF_SAADC_RESISTOR_DISABLED,         
+    .resistor_n = NRF_SAADC_RESISTOR_DISABLED,         
+    .gain       = NRF_SAADC_GAIN1_4,                   
+    .reference  = NRF_SAADC_REFERENCE_VDD4,        
+    .acq_time   = NRF_SAADC_ACQTIME_10US,              
+    .mode       = NRF_SAADC_MODE_SINGLE_ENDED,         
+    .pin_p      = (nrf_saadc_input_t)(NRF_SAADC_INPUT_AIN0),          
+    .pin_n      = NRF_SAADC_INPUT_DISABLED            
+    };
     err_code = nrf_drv_saadc_init(NULL, saadc_callback);
     APP_ERROR_CHECK(err_code);
 
@@ -186,25 +213,41 @@ int main(void)
         if (samples_ready) {
             nrf_drv_timer_pause(&m_timer);
             printf("\n\rSamples are ready to be used.\r\n");
+            static const int16_t noiseThreshold = 4;
             for (int i = 0; i < SAMPLES_IN_BUFFER; i++) {
-                testInput_f32_9khz[i*2] = (float)m_buffer_pool[0][i];
+                if (m_buffer_pool[0][i] > (512 - noiseThreshold) && m_buffer_pool[0][i] < (512 + noiseThreshold)) {
+                    testInput_f32_9khz[i*2] = 0;
+                } else {
+                    testInput_f32_9khz[i*2] = (float)(m_buffer_pool[0][i] - 512);  // Sign-convert for FFT; -512 to 512
+                }
+                //testInput_f32_9khz[i*2] = (float)m_buffer_pool[0][i];
                 //testInput_f32_9khz[i*2] = (i % 2) ? 800 : 400;
                 testInput_f32_9khz[i*2 + 1] = 0;
             }
-            
+//            
             arm_cfft_f32(&arm_cfft_sR_f32_len128, testInput_f32_9khz, /* ifftFlag */ 0, /* doBitReverse */ 1);
-            //arm_cfft_f32(&arm_cfft_sR_f32_len1024, testInput_f32_10khz, /* ifftFlag */ 0, /* doBitReverse */ 1);
+//            //arm_cfft_f32(&arm_cfft_sR_f32_len1024, testInput_f32_10khz, /* ifftFlag */ 0, /* doBitReverse */ 1);
             arm_cmplx_mag_f32(testInput_f32_9khz, testOutput, /* fftsize */ 128);
-            //arm_cmplx_mag_f32(testInput_f32_10khz, testOutput, /* fftsize */ 1024);
+//            //arm_cmplx_mag_f32(testInput_f32_10khz, testOutput, /* fftsize */ 1024);
             arm_max_f32(testOutput, 128, &maxValue, &testIndex);
             printf("\n\rMax Value: %f, at index: %d.\r\n", maxValue, testIndex);
-            for (int i = 0; i < SAMPLES_IN_BUFFER; i++) {
-                printf("\n\ri: %d, Value in testOutput: %f\r\n", i, testOutput[i]);
-                nrf_delay_ms(1);
-            }
             
-            nrf_delay_ms(100);
+            // Remove noise and apply EQ levels to testOutputArduinoAttmpt
+            uint8_t L = 0;
+            for (int i = 0; i < SAMPLES_IN_BUFFER/2; i++) {
+                // Make it match arduino output
+                testOutputArduinoAttmpt[i] = (uint32_t)((uint32_t)testOutput[i]/100);
+                //printf("%d:%d\r\n", i, testOutputArduinoAttmpt[i]);
+                
+                L = noise[i];
+                testOutputArduinoAttmpt[i] = (testOutputArduinoAttmpt[i] <= L) ? 0 :
+                    ((testOutputArduinoAttmpt[i] - L) * (256 - eq[i])) >> 8;
+                printf("%d:%d\r\n", i, testOutputArduinoAttmpt[i]);
+            }
+//            
+//            nrf_delay_ms(100);
             samples_ready = false;
+            nrf_drv_timer_resume(&m_timer);
         }
     }
 }
